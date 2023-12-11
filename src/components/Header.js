18e6a7f473c1db8d8b06083c685425cd1adb1ac2
raquelgm88 <raquel.rgm7@gmail.com>
2023-12-11 16:58:40 +0100

    Complete package.json file, callToApi function and Header component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header__title">INFO METEO</h1>
+        </header>
+    );
+};
+
+export default Header;
--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,10 @@
 // Fichero src/services/api.js
-const callToApi = () => {
+const callToApi = (codProv , idMun) => {
   // Llamamos a la API
-  return fetch('https://swapi.dev/api/people/5') // Este 5 es el id de Leia Skywalker
+  return fetch(`https://www.el-tiempo.net/api/json/v2/provincias/${codProv}/municipios/${idMun}`)
     .then((response) => response.json())
     .then((response) => {
-      // Cuando responde la API podemos limpiar los datos aquí
-      const result = {
-        name: response.name,
-        birthYear: response.birth_year,
-        height: response.height,
-        mass: response.mass,
-        eyeColor: response.eye_color,
-      };
-      return result;
+      return response;
     });
 };
 
