1bceaa96bcf110837e4f56a54597d4d61ccf0035
raquelgm88 <raquel.rgm7@gmail.com>
2023-12-14 13:24:29 +0100

    Modify callToApi function

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import callToApi from '../services/api';
+import callToApi from '../services/apis';
 import '../styles/App.scss';
 import Header from './Header';
 import Filters from './Filters';
@@ -9,13 +9,25 @@ function App() {
 
   //LLAMAR A LA API
   
-  const [starWarsData, setStarWarsData] = useState({});
+  const [provData, setProvData] = useState({});
+
+
+  const [datosDeLAProv, setDatosDeLAProv] = useState([]);
 
   useEffect(() => {
     // Dentro de useEffect llamamos a la API
     callToApi().then((response) => {
       // Cuando la API responde guardamos los datos en el estado para que se vuelva a renderizar el componente
-      setStarWarsData(response);
+      setProvData(response);
+    });
+    // Aquí ponemos un array vacío porque solo queremos que se llame a la API la primera vez
+  }, []);
+
+  useEffect(() => {
+    // Dentro de useEffect llamamos a la API
+    callToApi(codProv).then((response) => {
+      // Cuando la API responde guardamos los datos en el estado para que se vuelva a renderizar el componente
+      setDatosDeLAProv(response);
     });
     // Aquí ponemos un array vacío porque solo queremos que se llame a la API la primera vez
   }, []);
---
+++ src/services/apis.js
@@ -0,0 +1,50 @@
+// Fichero src/services/api.js
+
+const URL_PROV = 'https://www.el-tiempo.net/api/json/v2/provincias'
+
+// Obtener provincias
+callToApi()
+
+// Obtener tiempo provincia
+callToApi(codProv)
+
+// Obtener tiempo municipio
+callToApi(codProv, codMun)
+
+
+
+
+const callToApi = (codProv = '', codMun = '') => {
+  let url = URL_PROV;
+
+ if(codProv !== '' && codMun === '') {
+    url = `${url}/${codProv}`;
+    let urlMun = `${url}/municipios`;
+
+    const tiempoProv = fetch(url).then(response => response.json())
+    const listadoMun = fetch(urlMun).then(response => response.json())
+
+    return Promise.all([tiempoProv, listadoMun]).then(results => {
+      // aquí obtenemos un array con los resultados de cada promesa
+      const [resultProv, resultMun] = results
+      return results;
+    })
+
+  } else if(codProv !== '' && codMun !== '') {
+    url = `${url}/${codProv}/municipios/${codMun}`;
+  }
+
+// si los dos parámetros son string vacío, la url se devuelve tal cual por defecto
+// si codProv no es vacío, y codMun es vacío, url + codProv + /municipios
+// LLAMADA CUANDO SELECCIONO MUNICIPIO: cuando no estan vacíos nunguno, hago 3ª llamada
+
+
+  // Llamamos a la API
+  return fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+      return response;
+    });
+};
+
+export default callToApi;
