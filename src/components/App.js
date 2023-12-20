75e1a2121b642e9f2905c2e0a65b787e4d4d9eb4
raquelgm88 <raquel.rgm7@gmail.com>
2023-12-20 12:54:12 +0100

    Render mun select

--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,6 @@ function App() {
   useEffect(() => {
     callToApi().then((response) => {
       setProvData(response.provincias);
-
-      debugger;
     });
   }, []);
 
@@ -30,18 +28,15 @@ function App() {
 
 
   useEffect(() => {
-    debugger;
-    callToApi(provWeatherAndMunListData).then((response) => {
-      debugger;
+    callToApi(codProv).then((response) => {
       // tengo que hacer función que me muestre el tiempo de provincia y lueo me pinte el select con el listado de los municipios. La función la hago fuera y aquí la ejecuto.
       // Ver función use call back.
+      renderMunList();
     });
   }, [codProv]);
 
   useEffect(() => {
-    debugger;
     callToApi(provWeatherAndMunListData).then((response) => {
-      debugger;
       setProvWeatherAndMunListData(response);
     });
   }, [codProv]);
@@ -49,39 +44,63 @@ function App() {
   // FUNCIONES
 
   const renderProvList = () => {
-    const provSelect = document.querySelector('.filter__select');
-debugger;
+    const provSelect = document.querySelector('.main__filter--select');
     if(provSelect.innerHTML === '') {
       for  (let prov of provData) {
-        let  provOption = `<option className="filter__select--prov" value="${prov.CODPROV}">${prov.NOMBRE_PROVINCIA}</option>`;
+        let  provOption = `<option className="main__filter--select_prov" value="${prov.CODPROV}">${prov.NOMBRE_PROVINCIA}</option>`;
         provSelect.innerHTML += provOption;
       };
     }
   };
 
-  // renderProvList();
+  const renderMunList = () => {
+    const munForm = document.querySelector('.js_mun');
+    const label = `<label className="main__filter--label" htmlFor="mun">Selecciona un municipio</label>`;
+    const munSelect = `<select className="main__filter--select" name="mun" id="mun" onChange={handleMun} >
+    </select>`;
+    munForm.innerHTML = label + munSelect;
+    
+    if(munSelect.innerHTML === '') {
+      for  (let mun of codProv) {
+        let  munOption = `<option className="main__filter--select_mun" value="${mun.CODIGOINE}">${mun.NOMBRE}</option>`;
+        munSelect.innerHTML += munOption;
+      };
+    }
+  };
 
   const handleProv = (ev) => {
-    debugger;
     setCodProv(ev.target.value);
+    renderMunList();
   };
 
-
-  const renderMunList = () => {
-    
-  };
 
   return (
     <>
       <header className="header">
         <h1 className="header__title">INFO METEO</h1>
       </header>
-      <main>
-        <form className="filter">
-            <label className="filter__label" htmlFor="prov">Selecciona una provincia</label>
-            <select className="filter__select" name="prov" id="prov" onChange={handleProv} >
-            </select>
+      <main className='main'>
+        <form className="main__filter">
+          <label className="main__filter--label" htmlFor="prov">Selecciona una provincia</label>
+          <select className="main__filter--select" name="prov" id="prov" onChange={handleProv} >
+          </select>
         </form>
+        <form className='main__filter js_mun'>
+
+        </form>
+        <section className='main__container'>
+          <h2 className='main__container--title'>El tiempo en (PROVINCIA)</h2>
+          <div className='main__container--info'>
+            <div className='main__container--info_today'>
+              <h3 className='main__container--info_today-title'>Hoy</h3>
+              <p className='main__container--info_today-desc'>Descripción de hoy</p>
+            </div>
+            <div className='main__container--info_tomorrow'>
+              <h3 className='main__container--info_tomorrow-title'>Mañana</h3>
+              <p className='main__container--info_tomorrow-desc'>Descripción de mañana</p>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
